796a2ab9c94fafeb871450ff9031aea9de723ca1
Chris Taylor <christhebaron@gmail.com>
2016-04-16 18:47:53 +0100

    Removes whitespace in func

--- timeslot.go
+++ timeslot.go
@@ -14,7 +14,7 @@ type Show struct {
 	Title        string `json:"title"`
 	Desc         string `json:"desc"`
 	Photo        string `json:"photo"`
-	StartTimeRaw int64 `json:"start_time"`
+	StartTimeRaw int64  `json:"start_time"`
 	StartTime    time.Time
 	EndTimeRaw   int64 `json:"end_time"`
 	EndTime      time.Time
@@ -24,25 +24,18 @@ type Show struct {
 }
 
 func (s *Session) GetCurrentAndNext() (*CurrentAndNext, error) {
-
 	data, err := s.apiRequest("/timeslot/currentandnext", []string{})
-
 	if err != nil {
 		return nil, err
 	}
-
 	var currentAndNext CurrentAndNext
-
 	err = json.Unmarshal(*data, &currentAndNext)
-
 	if err != nil {
 		return nil, err
 	}
-
 	currentAndNext.Current.StartTime = time.Unix(currentAndNext.Current.StartTimeRaw, 0)
 	currentAndNext.Current.EndTime = time.Unix(currentAndNext.Current.EndTimeRaw, 0)
 	currentAndNext.Next.StartTime = time.Unix(currentAndNext.Next.StartTimeRaw, 0)
 	currentAndNext.Next.EndTime = time.Unix(currentAndNext.Next.EndTimeRaw, 0)
-
 	return &currentAndNext, nil
 }
