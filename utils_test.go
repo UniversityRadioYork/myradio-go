7ca11dd8a76d7ef4df52a052b620004759053dce
Charles Pigott <charlespigott@googlemail.com>
2016-05-11 20:34:30 +0100

    Use a table for tests

--- utils_test.go
+++ utils_test.go
@@ -6,22 +6,20 @@ import (
 )
 
 func TestParseDuration(t *testing.T) {
-	expected, err := time.ParseDuration("2h")
-	if err != nil {
-		t.Fail()
+	tests := []struct {
+		expectedStr string
+		time        string
+	}{
+		{"2h", "02:00:00"},
+		{"30m", "00:30:00"},
 	}
-	got, err := parseDuration("15:04:05", "02:00:00")
-	if got != expected {
-		t.Log("Got:", got, ", Expected:", expected)
-		t.Fail()
-	}
-	expected, err = time.ParseDuration("30m")
-	if err != nil {
-		t.Fail()
-	}
-	got, err = parseDuration("15:04:05", "00:30:00")
-	if got != expected {
-		t.Log("Got:", got, ", Expected:", expected)
-		t.Fail()
+
+	for _, test := range tests {
+		// We can safely leave testing the time lib to the stl
+		expected, _ := time.ParseDuration(test.expectedStr)
+		got, err := parseDuration("15:04:05", test.time)
+		if err != nil || got != expected {
+			t.Error("Got:", got, ", Expected:", expected, ", Error:", err)
+		}
 	}
 }
