417a41dbe1d0611ff87bd00eac23cf56e67df8a1
Brooke Hatton <hatton.brooke@gmail.com>
2016-10-09 00:16:50 +0100

    Initial banner setup
    -Supports Live Banners

---
+++ banner.go
@@ -0,0 +1,24 @@
+package myradio
+
+import (
+	"encoding/json"
+)
+
+type Banner struct {
+	BannerID	int 	`json:"banner_id"`
+	Alt			string 	`json:"alt"`
+	Target		string	`json:"Target"`
+	URL 		string	`json:"url"`
+}
+
+func (s *Session) GetLiveBanners() (banners []Banner, err error) {
+	data, err := s.apiRequest("/banner/livebanners/", []string{})
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(*data, &banners)
+	
+	return
+
+}
