4301d86bac8382b64fe2d4c08c4982d4b9c8b8ad
Brooke Hatton <hatton.brooke@gmail.com>
2016-10-11 22:48:23 +0100

    Add comments
    Pleases GOlint

--- banner.go
+++ banner.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 )
 
+// Banner represents the key information about banners
 type Banner struct {
 	BannerID int    `json:"banner_id"`
 	Alt      string `json:"alt"`
@@ -11,6 +12,8 @@ type Banner struct {
 	URL      string `json:"url"`
 }
 
+// GetLiveBanners gets the current live banners
+// and returns a sice of banners
 func (s *Session) GetLiveBanners() (banners []Banner, err error) {
 	data, err := s.apiRequest("/banner/livebanners/", []string{})
 	if err != nil {
