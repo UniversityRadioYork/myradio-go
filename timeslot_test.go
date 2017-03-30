ca1063bfebdfcc19c326b64d7a6344407643511f
mattbw <matt.windsor@ury.york.ac.uk>
2017-03-30 23:15:11 +0100

    Add rudimentary tests for new CAN methods
    These pass, but don't test much.

--- timeslot_test.go
+++ timeslot_test.go
@@ -4,8 +4,36 @@ import (
 	myradio "github.com/UniversityRadioYork/myradio-go"
 	"reflect"
 	"testing"
+	"time"
 )
 
+// testCanEntryZero tests whether a zero-valued CurrentAndNext entry returns true for IsZero.
+// It does NOT (yet) test the converse.
+func testCanEntryZero(t *testing.T) {
+	s := myradio.Show{}
+	if !s.IsZero() {
+		t.Error("zero show returns false for IsZero")
+	}
+}
+
+// testCanEntryEnds tests whether a CurrentAndNext entry returns something sensible for Ends.
+func testCanEntryEnds(t *testing.T) {
+	cases := []struct{
+		t time.Time
+		e bool
+	}{
+		{ t: time.Time{}, e: false },
+		{ t: time.Date(2009, time.April, 13, 11, 11, 11, 0, time.UTC), e: true },
+	}
+
+	for _, c := range cases {
+		s := myradio.Show{}
+		s.EndTime = c.t
+		if s.Ends() != c.e {
+			t.Error("show with end time", c.t, "gave incorrect Ends() of", s.Ends())
+		}
+	}
+}
 
 // testGetWeekScheduleZero tests whether GetWeekSchedule handles empty schedules correctly.
 func testGetWeekScheduleZero(t *testing.T) {
