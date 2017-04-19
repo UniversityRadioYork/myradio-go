ff42945609b2bb88968d39618f5c985de3c7379e
mattbw <matt.windsor@ury.york.ac.uk>
2017-04-20 00:13:07 +0100

    Test loading key from environment variable

--- api/key_test.go
+++ api/key_test.go
@@ -1,8 +1,11 @@
 package api
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
-func TestGetApiKeyFromFile(t *testing.T) {
+func TestGetAPIKeyFromFile(t *testing.T) {
 	var tests = []struct{ Path, Expected string }{
 		{"testdata/.myradio.key", "THIS-IS-A-TEST-KEY-THAT-WILL-NOT-WORK"},
 		{"testdata/.linebreaks.key", "THIS-KEY-HAS-SOME-LINE-BREAKS"},
@@ -15,5 +18,22 @@ func TestGetApiKeyFromFile(t *testing.T) {
 		if k != test.Expected {
 			t.Fatal("expected:", test.Expected, "got:", k)
 		}
+	}
+}
+
+func TestGetAPIKeyEnv(t *testing.T) {
+	os.Setenv("MYRADIOKEY", "foobar")
+	k, err := getAPIKeyEnv()
+	if err != nil {
+		t.Fatal("unexpected error:", err)
+	}
+	if k != "foobar" {
+		t.Fatal("expected foobar, got:", k)
+	}
+
+	os.Setenv("MYRADIOKEY", "")
+	_, nerr := getAPIKeyEnv()
+	if nerr != ErrNoMYRADIOKEY {
+		t.Fatal("expected error:", ErrNoMYRADIOKEY, "got error:", nerr)
 	}
 }
