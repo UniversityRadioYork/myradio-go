73d5117d0d25c01a375b9882d13729d44c5431c2
Matthew Stratford <matthew@mstratford.net>
2017-12-13 01:45:47 +0000

    Added API for single podcast lookups.

--- podcast.go
+++ podcast.go
@@ -1,5 +1,7 @@
 package myradio
 
+import "GitHub.com/UniversityRadioYork/myradio-go/api"
+
 // Podcast represents a podcast media item.
 type Podcast struct {
 	PodcastID     int `json:"podcast_id"`
@@ -10,6 +12,16 @@ type Podcast struct {
 	Photo         string
 	EditLink      Link `json:"editlink"`
 	MicrositeLink Link `json:"micrositelink"`
+}
+
+// Get retrieves the data for a single podcast from MyRadio given it's ID.
+// This consumes one API request.
+func (s *Session) Get(id int) (podcast *Podcast, err error) {
+	rq := api.NewRequestf("/podcast/%s", id)
+	if err = s.do(rq).Into(&podcast); err != nil {
+		return
+	}
+	return
 }
 
 // GetAllPodcasts retrieves the latest podcasts from MyRadio.
