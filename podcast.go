394fc9811a042c19e7bc50fb813ca2aa1331596c
Matthew Stratford <matthew@mstratford.net>
2017-12-19 01:42:29 +0000

    Update for further changes to Podcast API

--- podcast.go
+++ podcast.go
@@ -1,5 +1,11 @@
 package myradio
 
+import (
+	"strconv"
+
+	"github.com/UniversityRadioYork/myradio-go/api"
+)
+
 // Podcast represents a podcast media item.
 type Podcast struct {
 	PodcastID     int `json:"podcast_id"`
@@ -8,10 +14,9 @@ type Podcast struct {
 	Status        string
 	Time          Time
 	Photo         string
-	File          string `json:"file"`
-	WebFile       string
-	EditLink      Link `json:"editlink"`
-	MicrositeLink Link `json:"micrositelink"`
+	File          string `json:"uri"`
+	EditLink      Link   `json:"editlink"`
+	MicrositeLink Link   `json:"micrositelink"`
 }
 
 // Get retrieves the data for a single podcast from MyRadio given it's ID.
@@ -23,7 +28,17 @@ func (s *Session) Get(id int) (podcast *Podcast, err error) {
 
 // GetAllPodcasts retrieves the latest podcasts from MyRadio.
 // This consumes one API request.
-func (s *Session) GetAllPodcasts() (podcasts []Podcast, err error) {
-	err = s.get("/podcast/allpodcasts").Into(&podcasts)
+func (s *Session) GetAllPodcasts(noResults int, page int) (podcasts []Podcast, err error) {
+	//err = s.getf("/podcast/allpodcasts?noResults=%d", noResults, page).Into(&podcasts)
+
+	rq := api.NewRequest("/podcast/allpodcasts")
+	rq.Params["noResults"] = []string{strconv.Itoa(noResults)}
+	rq.Params["page"] = []string{strconv.Itoa(page)}
+	rs := s.do(rq)
+
+	if err := rs.Into(&podcasts); err != nil {
+		return nil, err
+	}
 	return
+
 }
