4db8b5bf883c23a340b3128d904515be8bbd0800
Marks Polakovs <github@markspolakovs.me>
2019-11-18 14:11:52 +0000

    Add endpoint to get all show subtypes

--- subtype.go
+++ subtype.go
@@ -5,3 +5,8 @@ type ShowSeasonSubtype struct {
 	Name      string `json:"name"`
 	Class     string `json:"class"`
 }
+
+func (s *Session) GetAllShowSubtypes() (subtypes []ShowSeasonSubtype, err error) {
+	err = s.get("/showSubtype/all").Into(&subtypes)
+	return
+}
