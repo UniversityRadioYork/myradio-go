d773ea3522574dab6ef5b8f7c0a664825db67f51
James Dolan <jgd511@york.ac.uk>
2020-09-25 19:51:27 +0100

    Add api methods getNext and getInRange for Events

---
+++ event.go
@@ -0,0 +1,38 @@
+package myradio
+
+import (
+	"github.com/UniversityRadioYork/myradio-go/api"
+	"strconv"
+)
+
+type Event struct {
+	ID          int `json:"id"`
+	Title       string
+	Description string
+	Start       string
+	End         string
+	Host        User
+}
+
+func (s *Session) GetEventsNext(n int) ([]Event, error) {
+	req := api.NewRequest("/event/next")
+	req.Params["n"] = []string{strconv.Itoa(n)}
+	var events []Event
+	err := s.do(req).Into(events)
+	if err != nil {
+		return nil, err
+	}
+	return events, nil
+}
+
+func (s *Session) GetEventsInRange(start string, end string) ([]Event, error) {
+	req := api.NewRequest("/event/inrange")
+	req.Params["start"] = []string{start}
+	req.Params["end"] = []string{end}
+	var events []Event
+	err := s.do(req).Into(events)
+	if err != nil {
+		return nil, err
+	}
+	return events, nil
+}
