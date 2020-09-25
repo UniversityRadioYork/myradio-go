39ba206b84bc80ff6154301440a7c38e3f7c4a60
James Dolan <jgd511@york.ac.uk>
2020-09-25 21:21:18 +0100

    Fix pointer error

--- event.go
+++ event.go
@@ -18,7 +18,7 @@ func (s *Session) GetEventsNext(n int) ([]Event, error) {
 	req := api.NewRequest("/event/next")
 	req.Params["n"] = []string{strconv.Itoa(n)}
 	var events []Event
-	err := s.do(req).Into(events)
+	err := s.do(req).Into(&events)
 	if err != nil {
 		return nil, err
 	}
@@ -30,7 +30,7 @@ func (s *Session) GetEventsInRange(start string, end string) ([]Event, error) {
 	req.Params["start"] = []string{start}
 	req.Params["end"] = []string{end}
 	var events []Event
-	err := s.do(req).Into(events)
+	err := s.do(req).Into(&events)
 	if err != nil {
 		return nil, err
 	}
