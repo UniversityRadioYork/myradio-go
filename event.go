3b95164b65508af32dc56c5fcb7bdcaf04fa7253
michael-grace <michael.dp.grace@outlook.com>
2022-02-06 02:39:26 +0000

    update to time types

--- event.go
+++ event.go
@@ -1,17 +1,31 @@
 package myradio
 
 import (
-	"github.com/UniversityRadioYork/myradio-go/api"
 	"strconv"
+	"time"
+
+	"github.com/UniversityRadioYork/myradio-go/api"
 )
 
 type Event struct {
-	ID          int `json:"id"`
-	Title       string
-	Description string
-	Start       string
-	End         string
-	Host        User
+	ID              int    `json:"id"`
+	Title           string `json:"title"`
+	HTMLDescription string `json:"description_html"`
+	StartTime       time.Time
+	StartTimeRaw    string `json:"start"`
+	EndTime         time.Time
+	EndTimeRaw      string `json:"end"`
+	Host            User   `json:"host"`
+}
+
+func (e *Event) populateEventTimes() (err error) {
+	e.StartTime, err = time.Parse(time.RFC3339, e.StartTimeRaw)
+	if err != nil {
+		return
+	}
+
+	e.EndTime, err = time.Parse(time.RFC3339, e.EndTimeRaw)
+	return
 }
 
 func (s *Session) GetEventsNext(n int) ([]Event, error) {
@@ -22,17 +36,33 @@ func (s *Session) GetEventsNext(n int) ([]Event, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	for k := range events {
+		err = events[k].populateEventTimes()
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	return events, nil
 }
 
-func (s *Session) GetEventsInRange(start string, end string) ([]Event, error) {
+func (s *Session) GetEventsInRange(start time.Time, end time.Time) ([]Event, error) {
 	req := api.NewRequest("/event/inrange")
-	req.Params["start"] = []string{start}
-	req.Params["end"] = []string{end}
+	req.Params["start"] = []string{start.String()}
+	req.Params["end"] = []string{end.String()}
 	var events []Event
 	err := s.do(req).Into(&events)
 	if err != nil {
 		return nil, err
 	}
+
+	for k := range events {
+		err = events[k].populateEventTimes()
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	return events, nil
 }
