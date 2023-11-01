cf6bb78851a4440fd27cfd0a9edd26cf367c2879
Michael Grace <michael.dp.grace@outlook.com>
2023-11-01 16:28:57 +0000

    training sessions

---
+++ training.go
@@ -0,0 +1,26 @@
+package myradio
+
+import (
+	"time"
+
+	"github.com/UniversityRadioYork/myradio-go/api"
+)
+
+type TrainingSession struct {
+	StartTimeRaw string `json:"demo_time"`
+	Host         string `json:"member"`
+	HostMemberID int    `json:"memberid"`
+}
+
+func (ts *TrainingSession) StartTime() time.Time {
+	t, _ := time.Parse("Mon 02 Jan 15:04", ts.StartTimeRaw)
+
+	return time.Date(time.Now().Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), 0, 0, time.Local)
+}
+
+func (s *Session) GetFutureTrainingSessions() (sessions []TrainingSession, err error) {
+	rq := api.NewRequestf("/demo/listdemos")
+	err = s.do(rq).Into(&sessions)
+
+	return
+}
