325814be9b9f07c1a044c4a8cc0986bc01eae73b
michael-grace <michael.dp.grace@outlook.com>
2024-05-29 15:16:51 +0100

    add term methods

---
+++ term.go
@@ -0,0 +1,30 @@
+package myradio
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/UniversityRadioYork/myradio-go/api"
+)
+
+// Term represents information about a MyRadio scheduling term
+type Term struct {
+	TermID int `json:"term_id"`
+	Start int `json:"start"`
+	Description string `json:"descr"`
+	NumWeeks int `json:"num_weeks"`
+	WeekNames []string `json:"week_names"`
+}
+
+// StartTime returns the start of the term as a time.Time object
+func (t *Term) StartTime() (time.Time) {
+	return time.Unix(t.Start, 0)
+}
+
+// GetAllTerms retrieves all the terms MyRadio is aware of (past and future)
+func (s *Session) GetAllTerms() (terms []Term, err error) {
+	err = s.get("/term/allterms/").Into(&terms)
+	return
+}
+
